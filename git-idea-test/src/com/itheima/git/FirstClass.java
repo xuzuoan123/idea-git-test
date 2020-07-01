eb9e773d4ea7c349a736df32646883b022b107da
XZA <942976635@qq.com>
2020-07-01 23:58:27 +0800

    提交新修改

--- git-idea-test/src/com/itheima/git/FirstClass.java
+++ git-idea-test/src/com/itheima/git/FirstClass.java
@@ -7,10 +7,15 @@ public class FirstClass {
         System.out.println("Hello idea-git-test");
         System.out.println("Hello idea-git-test");
         System.out.println("Hello idea-git-test");
+
+
+        // 添加其他的
+        System.out.println("--===fdsgfsfdesgfshg");
     }
 
 
     public void test() {
+        System.out.println("添加其他测试");
         System.out.println("A test func");
     }
 }
